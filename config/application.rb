0371a73463097c89a60295a6efda0b2e6b407d60
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-17 18:10:08 -0700

    Push view sections to be 100% of viewport

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,11 @@ Bundler.require(:default, Rails.env)
 
 module Portfolio
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
